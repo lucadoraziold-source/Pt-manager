74e2a9c690a7bc626e2c72b0b70c47e1faf9fd34
lucadoraziold-source <luca.doraziold@gmail.com>
2026-05-31 04:34:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // PT Manager SW v20 - network first, no stale cache
-const V = 'pt-v24';
+const V = 'pt-v25';
 self.addEventListener('install', function(e){ self.skipWaiting(); });
 self.addEventListener('activate', function(e){
   e.waitUntil(caches.keys().then(function(k){
